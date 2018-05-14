337ac8abd56ac3ede7796aba6e2c852af6fe81b1
Frank Garcia <omargg@gmail.com>
2018-05-14 16:03:32 -0700

    about lmri cleanup

--- partials/home/about-lmri-page.php
+++ partials/home/about-lmri-page.php
@@ -32,21 +32,6 @@ sort($pages);
             </li>
         
         <?php
-            // thumbnail with custom dimension 
-            // echo get_the_post_thumbnail( $page->ID, array( 260, 300));
-            // thumbnail url if needed
-           
-            // adds alt tag to thumbnail
-            // echo get_post_meta($page->ID, '_wp_attachment_image_alt', true);
-            // h3 post title
-            // echo '<h3>'.apply_filters( 'the_title', $page->post_title, $page->ID ).'</h3>';
-            // page content of partials
-            // echo apply_filters( 'the_content', $page->post_content );
-            // id if needed 
-            // echo 'ID'.$page->ID;
-            // if($canBeEdited) {
-            //     echo '<a href=" '.get_edit_post_link($page->ID, 'none').' ">Edit</a>';
-            // }
         ?>
         
     <?php endforeach; ?>
@@ -57,14 +42,3 @@ sort($pages);
 
 </section>
 
-
-<div class="d-flex flex-row">
-  <div class="p-2">Flex item 1</div>
-  <div class="p-2">Flex item 2</div>
-  <div class="p-2">Flex item 3</div>
-</div>
-<div class="d-flex flex-row-reverse">
-  <div class="p-2">Flex item 1</div>
-  <div class="p-2">Flex item 2</div>
-  <div class="p-2">Flex item 3</div>
-</div>
--- partials/home/hero-page.php
+++ partials/home/hero-page.php
@@ -1,10 +1,10 @@
 <div class="hero hero--home position--relative" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
-    <h1 class="position--absolute heading--center"><?php the_title(); ?></h1>
-    <!-- <div class="container">
+    <!-- <h1 class="position--absolute heading--center"><?php the_title(); ?></h1> -->
+    <div class="container">
         <div class="row">
             <div class="col">
                 <h1 class="position--absolute"><?php the_title(); ?></h1>
             </div>
         </div>
-    </div> -->
+    </div>
 </div>
