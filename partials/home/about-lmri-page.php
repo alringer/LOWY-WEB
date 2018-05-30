6346a7876d3a680442244815bd073cb41add8fa7
Frank Garcia <omargg@gmail.com>
2018-05-30 13:30:05 -0700

    fixed gitignore file

--- partials/home/about-lmri-page.php
+++ partials/home/about-lmri-page.php
@@ -9,34 +9,26 @@ sort($about_lmri_pages);
 ?>
  
 <section class="container">
-
- <div class="row">
-    <div class="col-12">
-        <ul class="list--grid-order home__card-grid about-card ">
-
-
-    <?php foreach ( $about_lmri_pages as $about_lmri_page ) : ?>
-
-            <li>
-                <?php echo '<img src="'.get_the_post_thumbnail_url($about_lmri_page->ID, 'post-thumbnail').'" class="img-responsive">'; ?>
-            </li>
-            <li class="position--relative home__card-grid__content-wrap">
-                <h3><?php echo apply_filters( 'the_title', $about_lmri_page->post_title, $about_lmri_page->ID )  ?></h3>
-                <?php echo '<p>'. apply_filters( 'the_excerpt', wp_trim_words( $about_lmri_page->post_content, 21, "" ) ) .'</p>'; ?>
-                <a href="<?php echo get_permalink($about_lmri_page->ID); ?>" class="read-more"> Read More  </a>
-                <div class=" ">
-                    <?php if($canBeEdited) {
-                        echo '<a href=" '.get_edit_post_link($about_lmri_page->ID, 'none').' ">Edit</a>';
-                    } ?>
-                </div>
-            </li>
-        
-        
-    <?php endforeach; ?>
-        </ul>
-
+    <div class="row">
+        <div class="col-12">
+            <ul class="list--grid-order home__card-grid about-card ">
+                <?php foreach ( $about_lmri_pages as $about_lmri_page ) : ?>
+                    <li>
+                        <?php echo '<img src="'.get_the_post_thumbnail_url($about_lmri_page->ID, 'post-thumbnail').'" class="img-responsive">'; ?>
+                    </li>
+                    <li class="position--relative home__card-grid__content-wrap">
+                        <h3><?php echo apply_filters( 'the_title', $about_lmri_page->post_title, $about_lmri_page->ID )  ?></h3>
+                        <?php echo '<p>'. apply_filters( 'the_excerpt', wp_trim_words( $about_lmri_page->post_content, 21, "" ) ) .'</p>'; ?>
+                        <a href="<?php echo get_permalink($about_lmri_page->ID); ?>" class="read-more"> Read More  </a>
+                        <div class=" ">
+                            <?php if($canBeEdited) {
+                                echo '<a href=" '.get_edit_post_link($about_lmri_page->ID, 'none').' ">Edit</a>';
+                            } ?>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </div>
-</div>
-
 </section>
 
