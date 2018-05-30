d118ed5bfbb108b89c277d7ddbc38793556cb4e0
Frank Garcia <omargg@gmail.com>
2018-05-30 16:12:52 -0700

    added grid styles

--- partials/home/clinical-research-page.php
+++ partials/home/clinical-research-page.php
@@ -1,9 +1,9 @@
-<?php $clinical_research_pages = get_pages( array( 'child_of' => 11 ) ); ?>
+<?php $clinical_research_page = get_page(11); ?>
 <?php
 // sorts by page added
-// sort($clinical_research_pages);
-$clinical_research_i = 0;
-$clinical_research_limit = 1;
+sort($clinical_research_pages);
+// $clinical_research_i = 0;
+// $clinical_research_limit = 1;
 ?>
 
 <?php 
@@ -11,17 +11,24 @@ $clinical_research_limit = 1;
 ?>
        
        
-<ul class="list--grid-order home__card-grid about-card ">
-    <?php foreach ( $clinical_research_pages as $clinical_research_page ) : ?>
-    <?php if($clinical_research_i < $clinical_research_limit) : ?>
+<ul class="list--unstyled list--grid-order-img-up home__card-grid about-card ">
+    <?php //foreach ( $clinical_research_pages as $clinical_research_page ) : ?>
+    <?php //if($clinical_research_i < $clinical_research_limit) : ?>
 
             <li>
                 <?php echo '<img src="'.get_the_post_thumbnail_url($clinical_research_page->ID, 'post-thumbnail').'" class="img-responsive">'; ?>
             </li>
+
             <li class="position--relative home__card-grid__content-wrap">
+
                 <h3><?php echo apply_filters( 'the_title', $clinical_research_page->post_title, $clinical_research_page->ID )  ?></h3>
-                <?php echo '<p>'. apply_filters( 'the_excerpt', wp_trim_words( $clinical_research_page->post_content, 21, "" ) ) .'</p>'; ?>
+
+                <?php
+                echo '<p>'. apply_filters( 'the_excerpt', wp_trim_words( $clinical_research_page->post_content, 20, "" ) ) .'</p>'; 
+                ?>
+
                 <a href="<?php echo get_permalink($clinical_research_page->ID); ?>" class="read-more"> Read More  </a>
+
                 <div class=" ">
                     <?php if($canBeEdited) {
                         echo '<a href=" '.get_edit_post_link($clinical_research_page->ID, 'none').' ">Edit</a>';
@@ -29,7 +36,20 @@ $clinical_research_limit = 1;
                 </div>
             </li>
         
-    <?php $clinical_research_i++; ?>
-        <?php endif ?>
-    <?php endforeach; ?>
+    <?php //$clinical_research_i++; ?>
+        <?php //endif ?>
+    <?php //endforeach; ?>
 </ul>
+
+
+<?php
+// $canBeEdited = current_user_can('editor') || current_user_can('administrator');
+
+//    $footer_id = 11; 
+//    $footer_p = get_page($footer_id);
+//    echo apply_filters('the_content', wp_trim_words( $footer_p->post_content, 21, "" ));
+
+//    if($canBeEdited) {
+//        echo '<a href=" '.get_edit_post_link($footer_id, 'none').' ">Edit</a>';
+//    } 
+?>
