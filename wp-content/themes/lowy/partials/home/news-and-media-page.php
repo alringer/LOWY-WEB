7571dd47797666c968bfbb210b1240e3da292082
Frank Garcia <omargg@gmail.com>
2018-06-05 16:19:20 -0700

    finished homepage grid section added sudo (tringles) to images

--- wp-content/themes/lowy/partials/home/news-and-media-page.php
+++ wp-content/themes/lowy/partials/home/news-and-media-page.php
@@ -6,7 +6,7 @@
 ?>
        
        
-<ul class="list  list--home-grid list--grid-order  list--unstyled     list--tablet-img-top list--reverse-order  home__card-grid  ">
+<ul class="list  list--home-grid list--grid-order  list--unstyled     list--tablet-img-top list--reverse-order  home__card-grid  list__news-media-content-no-pad">
     <?php //foreach ( $clinical_research_pages as $clinical_research_page ) : ?>
     <?php //if($clinical_research_i < $clinical_research_limit) : ?>
 
@@ -14,7 +14,7 @@
                 <?php echo '<img src="'.get_the_post_thumbnail_url($news_and_media_page->ID, 'post-thumbnail').'" class="img-responsive">'; ?>
             </li>
 
-            <li class="position--relative home__card-grid__content-wrap list__item-pad">
+            <li class="position--relative home__card-grid__content-wrap list__item-pad home__card-grid__content-wrap-left-arrow-news-media">
 
                 <h3><?php echo apply_filters( 'the_title', $news_and_media_page->post_title, $news_and_media_page->ID )  ?></h3>
 
