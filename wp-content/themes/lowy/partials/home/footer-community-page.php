35c31af93ab44759d8a2075f462500eaadbe7913
David Tello <dtello@seamgen.com>
2018-06-26 10:27:11 -0700

    adds fixes of stying for nav and footer

--- wp-content/themes/lowy/partials/home/footer-community-page.php
+++ wp-content/themes/lowy/partials/home/footer-community-page.php
@@ -1,6 +1,6 @@
 <?php 
     $canBeEdited = current_user_can('editor') || current_user_can('administrator');
-    $community_page_id = 17;
+    $community_page_id = 109;
     $community_pages = get_pages( array( 'child_of' => $community_page_id, 'sort_order' => 'desc', 'sort_column' => 'ID'  ) );
     $community_parent_page = get_page($community_page_id);
     $community_title = $community_parent_page->post_title;
@@ -8,9 +8,20 @@
 <?php $catquery = new WP_Query( 'cat=22&posts_per_page=2' ); ?>
 <h3 class="footer__heading"><?php echo apply_filters('post_title', $community_title) ?></h3>
 
-<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+<?php 
+    while($catquery->have_posts()) : $catquery->the_post();
+    $community_meta_img = get_post_meta($post->ID, 'alternate-hero-img', $single = true);
+    $community_img =  strlen($community_meta_img ) > 0 ? $community_meta_img :  '/wp-content/themes/lowy/img/default/default_bg.jpg';
+?>
 <div class="footer__community">
+    <div class="footer__community__thumb">
+        <img src="<?php echo $community_img; ?>" alt="">
+    </div>
     <div class="footer__community__desc">
+    
+    <?php
+    
+    ?>
         <h3 class="footer__subheading"><?php the_title(); ?></h3>
         <a href="<?php the_permalink() ?>" rel="bookmark" class="read-more">Read More <span class="icn icn--arrow"></span> </a>
     </div>
