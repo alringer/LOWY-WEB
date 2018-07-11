19266a9e4a80125f8c9311e67cc64ae313c46884
David Tello <dtello@seamgen.com>
2018-07-11 15:25:18 -0700

    refactor code and add LMRI STAff template to be used by sponsos, LMRI staff , BOD, SBOD

--- wp-content/themes/lowy/functions.php
+++ wp-content/themes/lowy/functions.php
@@ -549,4 +549,25 @@ wp_get_attachment_image( 1595 );
 }
 add_shortcode( 'bartag', 'bartag_func' );
 
+
+function add_taxonomies_to_pages() {
+    register_taxonomy_for_object_type( 'post_tag', 'page' );
+    register_taxonomy_for_object_type( 'category', 'page' );
+}
+add_action( 'init', 'add_taxonomies_to_pages' );
+
+if ( ! is_admin() ) {
+    add_action( 'pre_get_posts', 'category_and_tag_archives' );
+    
+}
+function category_and_tag_archives( $wp_query ) {
+    $my_post_array = array('post','page');
+    
+    if ( $wp_query->get( 'category_name' ) || $wp_query->get( 'cat' ) )
+    $wp_query->set( 'post_type', $my_post_array );
+    
+    if ( $wp_query->get( 'tag' ) )
+    $wp_query->set( 'post_type', $my_post_array );
+}
+
 ?>
---
+++ wp-content/themes/lowy/lmri-staff-page-template.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Template Name: LMRI Staff
+ */
+ get_header();
+ $lmri_staff_img_id = get_the_ID();
+ $lmri_staff_img_url = home_url();
+ $lmri_staff_img = get_post_meta($post->ID, 'alternate-hero-img', $single = true); 
+//  $diagnosing_mactels_content_img = get_post_meta($post->ID, 'reading-centers-content-img', $single = true); 
+?>
+<main role="main">
+	<!--  Hero section -->
+	<section>
+		<div class="hero hero--templates" style="background-image: url(<?php echo $lmri_staff_img_url;?><?php echo $lmri_staff_img; ?>)">
+		</div>
+	</section>
+	<!-- End Hero Section -->
+
+	<!-- template content section -->
+	<section class="single-page-template--content single-page-template--smaill-img-card">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<?php 
+					if ( function_exists('yoast_breadcrumb') ) {
+						yoast_breadcrumb('
+						<div class="breadcrumbs">','</div>
+						');
+					}
+				?>
+				<h1><?php the_title(); ?></h1>
+				<!-- Clinical research Post -->
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+					<!-- article -->
+						<article id="post-<?php the_ID(); ?>" class="single-page-template__article-content ">
+                            <?php the_content(); ?>	 
+
+							<br class="clear">
+							<?php edit_post_link(); ?>
+						</article>
+					<?php endwhile; ?>
+				<?php else: ?>
+ 				<!-- End Clinical research post -->
+
+
+				<!-- article -->
+				<article>
+					<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+				</article>
+				<!-- /article -->
+
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+	</section>
+	<?php 
+		$canBeEdited = current_user_can('editor') || current_user_can('administrator');
+		$lmri_staff_cat_id =  get_post_meta($post->ID, 'cat_id', true);;
+		$lmri_staff_cards_id = get_the_ID();
+		$lmri_staff_cat_query = new WP_Query( 'cat='.$lmri_staff_cat_id.'&order=asc');
+	?>
+	<section class="single-page-template--content ">
+		<div class="container">
+			<div class="row justify-content-center">
+				<?php while($lmri_staff_cat_query->have_posts()) : $lmri_staff_cat_query->the_post(); ?>
+					<div class="col-8 col-md-6 col-lg-4">
+					<div class="cards--staff" style="background-image: url('<?php echo get_the_post_thumbnail_url($post->ID, 'post-thumbnail'); ?>')">
+					<a href="<?php echo get_permalink($lmri_staff_cards_id->ID); ?>"><div class="cards--staff__img"></div></a>
+						<div class="cards--staff__content">
+							<div class="cards--staff__content__title text-center">
+								<h3 class="truncate"><?php the_title(); ?></h3>
+								<?php 
+									$bio_title = get_post_meta($post->ID, 'bio_title', true);
+								?>
+								<strong><?php echo wp_trim_words($bio_title, 5, "..." ); ?></strong>
+							</div>
+							<div class="text-center"><p><?php  echo /*the_excerpt(); */ '<p>'. wp_trim_words( $post->post_content, 32, "..." ). '</p>';  ?></p></div>
+							<div class="cards--staff__content__read-more text-center">
+								<a href="<?php echo get_permalink($lmri_staff_cards_id->ID); ?>" class="read-more-btn"> Read More  <span class=""></span></a>
+							</div>
+							<div class="can-edit-wrap">
+								<?php if($canBeEdited) {
+									echo '<a href=" '.get_edit_post_link($lmri_staff_cards_id->ID, 'none').' ">Edit</a>';
+								} ?>
+							</div>
+						</div>
+
+						
+
+					</div>
+					</div>
+				<?php endwhile; ?> 
+			</div>
+		</div>
+	</section>   
+
+	<!-- end template content section -->
+		
+</main>
+
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
