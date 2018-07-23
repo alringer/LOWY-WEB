63fa8440ca48816899197594c405df6100732e6a
Frank Garcia <fgarcia@seamgen.com>
2018-07-23 14:13:29 -0700

    started news page

--- wp-content/themes/lowy/news-template.php
+++ wp-content/themes/lowy/news-template.php
@@ -25,7 +25,9 @@
 						yoast_breadcrumb('
 						<div class="breadcrumbs">','</div>
 						');
-					}
+                    }
+                    //do something like this for order
+                    //$community_pages = get_pages( array( 'child_of' => $community_page_id, 'sort_order' => 'desc', 'sort_column' => 'ID'  ) );
 				?>
 				<h1><?php the_title(); ?></h1>
 				<!-- Clinical research Post -->
@@ -52,13 +54,80 @@
 			</div>
 		</div>
 	</div>
-	</section>
-	<?php 
-		$canBeEdited = current_user_can('editor') || current_user_can('administrator');
-		$lmri_staff_cat_id =  get_post_meta($post->ID, 'cat_id', true);;
-		$lmri_staff_cards_id = get_the_ID();
-		$lmri_staff_cat_query = new WP_Query( 'cat='.$lmri_staff_cat_id.'&order=asc');
-	?>
+    </section>
+    
+ 
+
+    <!-- end template content section -->
+    
+
+<?php 
+    $canBeEdited = current_user_can('editor') || current_user_can('administrator');
+    $featured_cat_id =  17;
+    $featured_cat_limit = 30;
+    $clinical_research_cards_id = get_the_ID();
+    $featured_catquery = new WP_Query( 'cat='.$featured_cat_id.'&posts_per_page='.$featured_cat_limit.'');
+?>
+
+<?php while($featured_catquery->have_posts()) : $featured_catquery->the_post(); ?>
+<section class="single-page-template--content ">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="cards__posts">
+                    <?php echo '<img src="'.get_the_post_thumbnail_url($post->ID, 'post-thumbnail').'" class="img-responsive">'; ?>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-8">
+                <div class="single-page-template__card-title">
+                    <h3><?php the_title(); ?></h3>
+                </div>
+                
+                <?php //echo '<p>'. apply_filters( 'the_content',( $clinical_research_cards_id->post_content ) ) .'</p>'; ?> 
+                
+                <?php echo '<p>'. strip_shortcodes( apply_filters( 'post_content', wp_trim_words( $post->post_content, 32, "" ) ) ) .'</p>';  ?>
+
+
+                <?php //echo '<p>'. apply_filters( 'post_content', wp_trim_words( $post->post_content, 32, "" ) ) .'</p>';  ?>
+
+                
+                <?php //echo get_post_field('post_content'); ?>
+
+                <div>
+                    <a href="<?php echo get_permalink($clinical_research_cards_id->ID); ?>" class="read-more-btn"> Read More  <span class=""></span></a>
+                </div>
+
+                <div class="can-edit-wrap">
+                    <?php if($canBeEdited) {
+                        echo '<a href=" '.get_edit_post_link($clinical_research_cards_id->ID, 'none').' ">Edit</a>';
+                    } ?>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+ </section>   
+
+ <?php get_template_part('partials/line', 'page'); ?>
+
+<?php endwhile; ?> 
+        
+
+
+
+
+
+
+
+
+<?php 
+    $canBeEdited = current_user_can('editor') || current_user_can('administrator');
+    $lmri_staff_cat_id =  get_post_meta($post->ID, 'cat_id', true);;
+    $lmri_staff_cards_id = get_the_ID();
+    $lmri_staff_cat_query = new WP_Query( 'cat='.$lmri_staff_cat_id.'&order=asc');
+?>
 	<section class="single-page-template--content ">
 		<div class="container">
 			<div class="row justify-content-center">
@@ -114,10 +183,10 @@
 				<?php endwhile; ?> 
 			</div>
 		</div>
-	</section>   
+	</section>  
 
-	<!-- end template content section -->
-		
+
+
 </main>
 
 
